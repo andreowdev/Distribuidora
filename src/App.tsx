8bce6abed7b5a8574bd77d107f9607cddea0cc06
andreowdev <developerandreo@gmail.com>
2025-02-09 18:04:24 -0300

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import SidebarMain from "./screens/sidebar/components/Sidebar"
+
+
+function App() {
+    return (
+        <div>
+            <SidebarMain />
+        </div>
+    )
+  }
+  
+  export default App
